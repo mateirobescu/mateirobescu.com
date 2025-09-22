ca24404412cfbc651c669a00e90458ef187e84fc
Matei Robescu <robescumatei@Mateis-MacBook-Pro.local>
2025-09-22 16:14:53 +0300

    Added superuser

--- backend/mateirobescu/settings.py
+++ backend/mateirobescu/settings.py
@@ -31,13 +31,13 @@ ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 # Application definition
 
 INSTALLED_APPS = [
-    'cloudinary_storage',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cloudinary_storage',
     'cloudinary',
     'portfolio',
 ]
--- backend/mateirobescu/wsgi.py
+++ backend/mateirobescu/wsgi.py
@@ -14,3 +14,18 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mateirobescu.settings')
 
 application = get_wsgi_application()
+
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+username = os.getenv("USER", "admin")
+email = os.getenv("EMAIL", "admin@example.com")
+password = os.getenv("PASSWORD", "changeme123")
+
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(
+        username=username,
+        email=email,
+        password=password
+    )
